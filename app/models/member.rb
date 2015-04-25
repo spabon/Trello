ea270db23e7749b509fc82328585155c4c4f4309
dmromero <dmromero@uninorte.edu.co>
2015-04-25 17:58:47 +0000

    Validaciones

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,6 @@
 class Member < ActiveRecord::Base
-    has_many :task
+    has_many :tasks
+    validates :name, presence: true
+    validates :lastname, presence: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
     belongs_to :member
+    
+    validates :title, :description, presence: true
+    validates :duration, numericality: { only_integer: true }
 end
